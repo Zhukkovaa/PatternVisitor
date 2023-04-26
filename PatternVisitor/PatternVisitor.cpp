48a96b32f3eeabc6ed80083d5062aebc9a73e9c3
Katya Zhukova <gjugjuka2002@gmail.com>
2023-04-26 19:32:10 +0400

    Реализована иерархия классов "Выражение"->("Число", "Бинарная операция","Вызов функции","Переменная")
    Бинарная операция подразумевает возможность выполнения сложения, умножения, вычистания, деления. Вызов функции подразумевает работу с вызовом sqrt (квадратный корень) и abs (модуль). У переменной есть имя.
    
    В файле D:\PatternVisitor\PatternVisitor\UnitTest1\UnitTest1.cpp находятся юнит тесты для проверки представленных структур

---
+++ PatternVisitor/PatternVisitor.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>//подключаем библиотеку ввода/вывода
+#include <cassert>//подключаем библиотеку для проверки утверждений (assertions)    
+#include <string>//подключаем библиотеку для работы со строками 
+using namespace std;//используем пространство имен std
+
+//Пусть имеется иерархия классов «Выражение» → («Число», «Бинарная операция», «Вызов
+//функции», «Переменная»), описанные ниже.Бинарная операция подразумевает возможность выполнения сложения, умножения,
+//вычитания и деления; вызов функции — подразумевает работу с вызовом sqrt(квадратный
+//корень) и abs(модуль).У переменной есть имя(значение пока ей не присваиваем).
+
+struct Number;//объявляем структуру Number
+struct BinaryOperation;//объявляем структуру BinaryOperation
+struct FunctionCall;//объявляем структуру FunctionCall
+struct Variable;//объявляем структуру Variable
+
+struct Expression //объявляем базовую абстрактную структуру Expression
+{
+	virtual ~Expression() { }//объявляем виртуальный деструктор для базовой структуры Expression
+	virtual double evaluate() const = 0; //абстрактный метод «вычислить»  (объявляем чистый виртуальный метод evaluate() для базовой структуры Expression)
+	virtual std::string print() const = 0;//абстрактный метод "печать"
+};
+struct Number : Expression // стуктура «Число»
+{
+	Number(double value) : value_(value) {} //конструктор
+	double value() const { return value_; } // метод чтения значения числа
+	double evaluate() const { return value_; } // реализация виртуального метода «вычислить»
+	~Number() {}//деструктор, тоже виртуальный
+	//Метод print() возвращает строковое представление числового значения объекта класса Number.
+	//Он использует функцию std::to_string() для преобразования числового значения value_ в строку и возвращает эту строку.
+	std::string print() const {
+		return std::to_string(this->value_);
+	}
+
+private:
+	double value_; // само вещественное число
+};
+struct BinaryOperation : Expression // «Бинарная операция»
+{
+	//  Этот фрагмент кода объявляет перечисление, которое связывает символы математических операций с числовыми константами.
+	//  PLUS = +: Константа PLUS имеет значение + и будет использоваться для обозначения операции сложения.
+	//	MINUS = -: Константа MINUS имеет значение - и будет использоваться для обозначения операции вычитания.
+	//	DIV = / : Константа DIV имеет значение / и будет использоваться для обозначения операции деления.
+	//	MUL = *: Константа MUL имеет значение * и будет использоваться для обозначения операции умножения.
+	enum { // перечислим константы, которыми зашифруем символы операций
+		PLUS = '+',
+		MINUS = '-',
+		DIV = '/',
+		MUL = '*'
+	};
+	//Этот код определяет конструктор для BinaryOperation класса, который принимает три аргумента: left, op и right.
+	//left и right являются указателями на Expression объекты, представляющие левый и правый операнды двоичной операции 
+	//соответственно.op - это целочисленный код для операции, которая должна быть выполнена.Коды определены с использованием
+	//перечисления, приведенного ранее в коде, где PLUS присваивается + , MINUS to - , DIV to / и MUL to * .
+	//Конструктор инициализирует переменные - члены класса left_, op_ и right_ соответствующими аргументами.
+	// Он также включает утверждение, что left_ и right_ являются ненулевыми указателями, указывающими на то, что 
+	// конструктор ожидает передачи допустимых выражений.Если left_ или right_ является нулевым указателем, утверждение 
+	// завершится ошибкой, и программа завершится с сообщением об ошибке.
+	// в конструкторе надо указать 2 операнда — левый и правый, а также сам символ операции
+	BinaryOperation(Expression const* left, int op, Expression const* right) : left_(left), op_(op), right_(right)
+	{
+		assert(left_ && right_);
+	}
+	~BinaryOperation() //в деструкторе освободим занятую память
+	{
+		delete left_;
+		delete right_;
+	}
+	Expression const* left() const { return left_; } // чтение левого операнда
+	Expression const* right() const { return right_; } // чтение правого операнда
+	int operation() const { return op_; } // чтение символа операции
+	double evaluate() const // реализация виртуального метода «вычислить»
+	{
+		double left = left_->evaluate(); // вычисляем левую часть
+		double right = right_->evaluate(); // вычисляем правую часть
+		switch (op_) // в зависимости от вида операции, складываем, вычитаем, или делим левую и правую части
+		{
+		case PLUS: return left + right;
+		case MINUS: return left - right;
+		case DIV: {
+			if (right == 0.0) {
+				throw std::invalid_argument("Division by zero");
+			}
+			else {
+				return left / right;
+			}
+		}
+		case MUL: return left * right;
+		}
+	}
+	std::string print() const {
+		return this->left_->print() + std::string(1, this->op_) + this->right_->print();
+	}
+private:
+	Expression const* left_; // указатель на левый операнд
+	Expression const* right_; // указатель на правый операнд
+	int op_; // символ операции
+};
+struct FunctionCall : Expression // структура «Вызов функции»
+{
+	//Этот код описывает конструктор класса FunctionCall, который создает объект для представления вызова функции в математическом выражении.
+	//Аргументами конструктора являются имя функции в виде строки name и аргумент arg в виде указателя на объект класса Expression.
+	//В конструкторе также есть два вызова функции assert, которые выполняют проверки утверждений.
+	//Первый assert проверяет, что arg не является нулевым указателем, чтобы избежать возможных ошибок во время выполнения программы.
+	//Второй assert проверяет, что имя функции является корректным.В данном случае поддерживаются только функции "sqrt" и "abs".Если имя функции не соответствует этим значениям, то программа прерывается с сообщением об ошибке.
+	// в конструкторе надо учесть имя функции и ее аргумент
+	FunctionCall(std::string const& name, Expression const* arg) : name_(name),
+		arg_(arg)
+	{
+		assert(arg_);
+		assert(name_ == "sqrt" || name_ == "abs");
+	} // разрешены только вызов sqrt и abs
+	std::string const& name() const
+	{
+		return name_;
+	}
+	Expression const* arg() const // чтение аргумента функции
+	{
+		return arg_;
+	}
+	~FunctionCall() { delete arg_; } // освобождаем память в деструкторе
+	virtual double evaluate() const { // реализация виртуального метода
+		//«вычислить»
+		if (name_ == "sqrt")
+			if (arg_->evaluate() < 0.0) {
+				throw std::invalid_argument("Square root of negative number");
+			}
+			else {
+				return sqrt(arg_->evaluate()); // либо вычисляем корень квадратный}
+			}
+		else return fabs(arg_->evaluate());
+	} // либо модуль — остальные функции запрещены
+	std::string print() const {
+		return this->name_ + "(" + this->arg_->print() + ")";
+	}
+private:
+	std::string const name_; // имя функции
+	Expression const* arg_; // указатель на ее аргумент
+};
+struct Variable : Expression // структура «Переменная»
+{
+	Variable(std::string const& name) : name_(name) { } //в конструкторе надо
+	//указать ее имя
+	std::string const& name() const { return name_; } // чтение имени переменной
+	double evaluate() const // реализация виртуального метода «вычислить»
+	{
+		return 0.0;
+	}
+	std::string print() const {
+		return this->name_;
+	}
+private:
+	std::string const name_; // имя переменной
+};
+
+int main()
+{
+	//------------------------------------------------------------------------------
+	Expression* e1 = new Number(1.234);
+	Expression* e2 = new Number(-1.234);
+	Expression* e3 = new BinaryOperation(e1, BinaryOperation::DIV, e2);
+	cout << e3->evaluate() << endl;
+	//------------------------------------------------------------------------------
+	Expression* n32 = new Number(32.0);
+	Expression* n16 = new Number(16.0);
+	Expression* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+	Expression* callSqrt = new FunctionCall("sqrt", minus);
+	Expression* n2 = new Number(2.0);
+	Expression* mult = new BinaryOperation(n2, BinaryOperation::MUL, callSqrt);
+	Expression* callAbs = new FunctionCall("abs", mult);
+	cout << callAbs->evaluate() << endl;
+	//
+}
+
---
+++ PatternVisitor/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,125 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\PatternVisitor.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		//тест на проверку работы конструктора класса Number
+		TEST_METHOD(TestConstuctorNumber)
+		{
+			Number* n32 = new Number(32.0);
+			Assert::IsTrue(n32->value() == 32.0);
+		}
+		//проверка виртуального метода «вычислить» класса Number
+		TEST_METHOD(TestEvaluateNumber)
+		{
+			Number* n32 = new Number(32.0);
+			Assert::IsTrue(n32->evaluate() == 32.0);
+		}
+		//тест для метода print() класса Number
+		TEST_METHOD(TestPrintNumber)
+		{
+			Number* n32 = new Number(32.0);
+			Assert::IsTrue(n32->print() == to_string(32.0));
+		}
+		//тест на проверку работы конструктора класса BinOperation
+		TEST_METHOD(TestConstuctorBinOperation)
+		{
+			Number* n32 = new Number(32.0);
+			Number* n16 = new Number(16.0);
+			BinaryOperation* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+			Assert::IsTrue(minus->left() == n32 && minus->operation() == BinaryOperation::MINUS && minus->right() == n16);
+
+		}
+		//тест на проверку метода evaluate() класса BinOperation для вычитания
+		TEST_METHOD(TestBinOperMinus) {
+			Number* n32 = new Number(32.0);
+			Number* n16 = new Number(16.0);
+			BinaryOperation* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+			Assert::IsTrue(minus->evaluate() == 16.0);
+		}
+		//тест на проверку метода evaluate() класса BinOperation для сложения
+		TEST_METHOD(TestBinOperPlus) {
+			Number* n32 = new Number(-16.0);
+			Number* n16 = new Number(16.0);
+			BinaryOperation* plus = new BinaryOperation(n32, BinaryOperation::PLUS, n16);
+			Assert::IsTrue(plus->evaluate() == 0.0);
+		}
+		//тест на проверку метода evaluate() класса BinOperation для деления
+		TEST_METHOD(TestBinOperDiv) {
+			Number* n32 = new Number(32.0);
+			Number* n16 = new Number(16.0);
+			BinaryOperation* div = new BinaryOperation(n32, BinaryOperation::DIV, n16);
+			Assert::IsTrue(div->evaluate() == 2.0);
+		}
+		//тест на проверку метода evaluate() класса BinOperation для деления на 0
+		TEST_METHOD(TestBinOperDivByZero) {
+			Number* n32 = new Number(32.0);
+			Number* n0 = new Number(0.0);
+			BinaryOperation* divzero = new BinaryOperation(n32, BinaryOperation::DIV, n0);
+			Assert::ExpectException<std::invalid_argument>([divzero]() {divzero->evaluate(); });
+
+		}
+		//тест на проверку метода evaluate() класса BinOperation для умножения
+		TEST_METHOD(TestBinOperMul) {
+			Number* n32 = new Number(32.0);
+			Number* n16 = new Number(16.0);
+			BinaryOperation* mul = new BinaryOperation(n32, BinaryOperation::MUL, n16);
+			Assert::IsTrue(mul->evaluate() == 512.0);
+		}
+		//тест на проверку метода print класса BinOperation
+		TEST_METHOD(TestBinOperPrint) {
+			Number* n32 = new Number(32.0);
+			Number* n16 = new Number(16.0);
+			BinaryOperation* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+			Assert::IsTrue(minus->print() == to_string(32.0) + string(1, BinaryOperation::MINUS) + to_string(16.0));
+		}
+		//тест для конструктора класса FunctionCall
+		TEST_METHOD(TestConstructorFuncCall) {
+			Number* n16 = new Number(16.0);
+			FunctionCall* callSqrt = new FunctionCall("sqrt", n16);
+			Assert::IsTrue(callSqrt->arg() == n16 && callSqrt->name() == "sqrt");
+
+		}
+		//тест для проверки метода Evalute при вычислении квадратного корня положит-го числа для класса FunctionCall
+		TEST_METHOD(TestEvaluteFuncCallSQRT) {
+			Number* n16 = new Number(16.0);
+			FunctionCall* callSqrt = new FunctionCall("sqrt", n16);
+			Assert::IsTrue(callSqrt->evaluate() == 4.0);
+
+		}
+		//тест для проверки метода Evalute при вычислении квадратного корня для отрицат-го числа класса FunctionCall
+		TEST_METHOD(TestEvaluteNegativeSQRTFuncCall) {
+			Number* n16 = new Number(-16.0);
+			FunctionCall* callSqrt = new FunctionCall("sqrt", n16);
+			Assert::ExpectException<std::invalid_argument>([callSqrt]() {callSqrt->evaluate(); });
+
+		}
+		//тест для проверки метода Evalute при вычислении модуля для отрицательного числа класса FunctionCall
+		TEST_METHOD(TestEvaluteFuncCallABS) {
+			Number* n16 = new Number(-16.0);
+			FunctionCall* callSqrt = new FunctionCall("abs", n16);
+			Assert::IsTrue(callSqrt->evaluate() == 16.0);
+
+		}
+		//тест для конструктора класса Value
+		TEST_METHOD(TestConstructorValue) {
+			Variable* var = new Variable("var");
+			Assert::IsTrue(var->name() == "var");
+		}
+		//тест на проверку метода print класса Value
+		TEST_METHOD(TestPrintValue) {
+			Variable* var = new Variable("var");
+			Assert::IsTrue(var->print() == "var");
+		}
+		//тест виртуального метода «вычислить»:
+		TEST_METHOD(TestEvaluateValue) {
+			Variable* var = new Variable("var");
+			Assert::IsTrue(var->evaluate() == 0.0);
+		}
+	};
+}
